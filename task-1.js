699c4353e334924e38c76d156d3cd2ba83d0b5c7
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2023-10-30 15:28:58 +0000

    update all

--- task-1.js
+++ task-1.js
@@ -2,3 +2,8 @@ function slugify (title){
     title = title.toLowerCase().replace('-');
     return title;
 }
+
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
