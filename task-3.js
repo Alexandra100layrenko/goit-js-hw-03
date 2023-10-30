2f4cdb084426dff2569077bf706d13250dab99bc
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2023-10-30 16:17:07 +0000

    update all

--- task-3.js
+++ task-3.js
@@ -1,9 +1,15 @@
 function filterArray(numbers, value){
     let tfilteredNumbers = [];
-  /*for (const = i; i > numbers.length; i++){}*/
+  //for (const = i; i > numbers.length; i++){}//
     for (const element of filterArray){
         if (numbers[i] > value){
             filteredNumbers.push(numbers[i]);
         }
     }
 }
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
